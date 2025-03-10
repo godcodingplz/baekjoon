790cb859dba0447e21c782bcfde20596ff1c4c3e
godcodingplz <155733434+godcodingplz@users.noreply.github.com>
2025-03-10 11:41:20 +0900

    [Silver II] Title: 최소 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.cc
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    
+    priority_queue<int, vector<int>, greater<int>> queue;
+
+    int n;
+    cin >> n;
+
+    for(int i=0; i<n; i++){
+        int x; cin >> x;
+        if(x == 0){
+            if(!queue.empty()){
+                cout << queue.top() << '\n';
+                queue.pop();
+            }
+            else{
+                cout << 0 << '\n';
+            }
+        }
+        else{
+            queue.push(x);
+        }
+    }
+    return 0;
+}
